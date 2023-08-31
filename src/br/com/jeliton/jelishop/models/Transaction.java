9ce6dea1b5cc719f6fd1442c681276d919833e5c
JelitonSoares <jelitonn.soarres@gmail.com>
2023-08-30 21:19:38 -0300

    feat: Transaction class

---
+++ src/br/com/jeliton/jelishop/models/Transaction.java
@@ -0,0 +1,23 @@
+package br.com.jeliton.jelishop.models;
+
+public class Transaction {
+
+    private String name;
+    private double price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
