fb2e4a76bee3f5ca8da358fe237b3c1410a68bbf
JelitonSoares <jelitonn.soarres@gmail.com>
2023-08-30 21:19:21 -0300

    feat: User class

---
+++ src/br/com/jeliton/jelishop/models/User.java
@@ -0,0 +1,18 @@
+package br.com.jeliton.jelishop.models;
+
+public class User {
+
+    private double credit;
+
+    public User (double credit) {
+        this.credit = credit;
+    }
+
+    public double getCredit () {
+        return this.credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+}
